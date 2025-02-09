440c045ed2c1f8dfd57cd8914a8f1fcb00b9c6ad
Satvik R Shetty <satvikrshetty423@gmail.com>
2025-02-09 16:23:22 +0530

    [code-V-9] Understanding Laravel Seeders

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\student;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,6 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        student::factory()->count(50)->create();
     }
 }
