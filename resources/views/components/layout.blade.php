35bfc246762be6dbdd151618d1da006f0f9e5185
Satvik R Shetty <satvikrshetty423@gmail.com>
2025-02-10 11:07:50 +0530

    [code-V-11] Understanding Laravel Named Routes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -14,8 +14,8 @@
     <header class="sticky top-0 h-fit">
         <nav>
             <h1>Student Network</h1>
-            <a href="/students"> All students </a>
-            <a href="/students/create"> Add New Students </a>
+            <a href="{{ route('students.index') }}"> All students </a>
+            <a href="{{ route('students.create') }}"> Add New Students </a>
         </nav>
     </header>
 
--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -4,7 +4,7 @@
 <ul>
     @foreach ($students as $student)
                 <li>
-                   <x-card href="/students/{{ $student->id }}" :highlight-red='$student["mark"] < 40' :highlight-blue='$student["mark"] >= 40 && $student["mark"] < 60' :highlight-green='$student["mark"] >= 60 && $student["mark"] < 80' :highlight-gold='$student["mark"] >= 80'>
+                   <x-card href="{{ route('students.show', $student->id) }}" :highlight-red='$student["mark"] < 40' :highlight-blue='$student["mark"] >= 40 && $student["mark"] < 60' :highlight-green='$student["mark"] >= 60 && $student["mark"] < 80' :highlight-gold='$student["mark"] >= 80'>
                         <h3>{{ $student->name }}</h3>
                    </x-card>
                 </li>
