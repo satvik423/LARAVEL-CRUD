58180d1ae000895dd0b32cc1f4e92a0d7c106883
Satvik R Shetty <satvikrshetty423@gmail.com>
2025-02-12 11:58:10 +0530

    Project Finished

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
     <h2> {{ $student->name }} </h2>
-    <div class="bg-gray-200 p-4 rounded">
+    <div class="show">
         <p><strong>Class: </strong> {{ $student->class }} </p>
         <p><strong>Mark: </strong> {{ $student->mark }} </p>
         <p><strong>About me: </strong> {{ $student->bio }} </p>
